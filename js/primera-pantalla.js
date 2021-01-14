318f418b02d792469e9f367ec670b762e9673304
Hernan.Reiq <hernandemure1202@gmail.com>
2021-01-14 03:28:57 -0400

    Estetica basica de la pantalla de pelea realizada, se agrego musica dependiendo la pantalla y se crearon los objetos que tendran las propiedades y metodos de los personajes

--- js/primera-pantalla.js
+++ js/primera-pantalla.js
@@ -6,19 +6,44 @@ musica.volume = 0.15;
 musica.style.display = "none";
 
 var bienvenida = document.createElement('p');
-bienvenida.innerText = "Te doy la bienvenida a mi simulador de una batalla Pokémon";
+bienvenida.innerText = "Te doy la bienvenida a mi simulador de batalla";
 primera_pantalla.appendChild(bienvenida);
 
 var boton_iniciar = document.createElement('button');
 boton_iniciar.innerText = "Vamos a pelear";
 primera_pantalla.appendChild(boton_iniciar);
 
-
 boton_iniciar.addEventListener('click', cambiarPantalla);
 
 function cambiarPantalla() {
     musica.src = "audio/pelea.mp3";
-    primera_pantalla.style.display = "none";
-    controles.style.display = "block";
-    document.getElementById('pantalla-2').style.display = "block";
+    primera_pantalla.classList.add('no-disponible');
+    document.getElementById('pantalla-2').classList.remove('no-disponible');
+    setTimeout(function() {
+        controles.style.display = "block";
+    }, 3);
 }
+
+var atacar = document.getElementById('atacar');
+var persona_img = document.getElementById('personaje');
+var oponente_img = document.getElementById('oponente');
+
+var personaje = {
+    salud: 100,
+    defensa: 30,
+    ataque: 3,
+    turno: false,
+    atacar: function() {
+        oponente.salud = oponente.salud - this.ataque;
+    }
+};
+
+var oponente = {
+    salud: 100,
+    defensa: 30,
+    ataque: 3,
+    turno: false,
+    atacar: function() {
+        personaje.salud = personaje.salud - this.ataque;
+    }
+};
